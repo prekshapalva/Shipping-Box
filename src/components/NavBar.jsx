fb60c8a7b77145c521ff3e217e3d5c256490a5ee
prekshapalva <prekshap.is19@sahyadri.edu.in>
2021-01-19 11:37:09 +0530

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBCollapse,
+  } from "mdbreact";
+  
+  function NavBar(props) {
+    return (
+      <div>
+      <MDBNavbar color="indigo" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Shipping Box</strong>
+        </MDBNavbarBrand>
+        <MDBCollapse id="navbarCollapse3" navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBNavLink
+                className="waves-effect waves-light"
+                to={props.navLink.path}
+              >
+                {props.navLink.Lable}
+              </MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
